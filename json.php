66b6a85bf146c437b75258c3d0b78ae82c6f599f
iooner <contact@iooner.be>
2018-09-24 15:45:11 +0200

    json draft

--- json.php
+++ json.php
@@ -1,4 +1,14 @@
 <?php
 $data = array(
-	"door" => $status,
+	"version" => "0.01",
+	"areas" => array(
+		"openspace" => array("window" => "","door" => ""),
+		"workshop" => array("window" => "","door" => ""),
+		"convi" => array("window" => "","door" => ""),
+		"storage" => array(),
+		"lab" => array("window" => "","door" => ""),
+		"hallway" => array()
+	),
 );
+
+?>
